a93f3d36a2962872b877db4b2ad4bbac3997f2f1
song_Tonny <13373338337@163.com>
2019-09-02 23:15:01 +0800

    flink-dataset 求和倒序排序

--- src/main/scala/chaoyue/Han/flinkDataSet/FlinkWC.scala
+++ src/main/scala/chaoyue/Han/flinkDataSet/FlinkWC.scala
@@ -1,7 +1,9 @@
 package chaoyue.Han.flinkDataSet
 
 import org.apache.flink.api.common.operators.Order
+import org.apache.flink.api.java.aggregation.Aggregations
 import org.apache.flink.api.scala.ExecutionEnvironment
+import org.elasticsearch.search.aggregations.InternalOrder.Aggregation
 //必须导入的隐式转换
 import org.apache.flink.api.scala._
 
@@ -13,11 +15,15 @@ object FlinkWC {
 
     val textDataSet = env.readTextFile("E:\\a.json")
 
-    textDataSet.flatMap(_.split(","))
+    val result = textDataSet.flatMap(_.split(","))
       .map((_, 1))
       .groupBy(0)
       .sum(1)
-      .print()
+
+    result.collect().sortBy(-_._2).foreach(println(_))
+
+
+    //    result.print()
 
 
   }
